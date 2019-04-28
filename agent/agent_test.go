9cfb7a5cd45df619b4063afc767db1f9fdfa6f37
Hans Hasselberg <me@hans.io>
2019-04-29 01:06:23 +0200

    feedback

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -3776,6 +3776,7 @@ func TestAgent_ReloadConfigOutgoingRPCConfig(t *testing.T) {
 
 	hcl = `
 		data_dir = "` + dataDir + `"
+		connect = { enabled = false }
 		verify_outgoing = true
 		ca_path = "../test/ca_path"
 		cert_file = "../test/key/ourdomain.cer"
@@ -3796,6 +3797,7 @@ func TestAgent_ReloadConfigIncomingRPCConfig(t *testing.T) {
 	defer os.RemoveAll(dataDir)
 	hcl := `
 		data_dir = "` + dataDir + `"
+		connect = { enabled = false }
 		verify_outgoing = true
 		ca_file = "../test/ca/root.cer"
 		cert_file = "../test/key/ourdomain.cer"
